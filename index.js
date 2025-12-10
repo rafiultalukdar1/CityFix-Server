6bef1064b189e04f62142df9674293ef674c93e4
rafi0012 <rafiultalukdar007@gmail.com>
2025-12-10 12:48:34 +0600

    Implement search & filters for issues

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 require('dotenv').config();
@@ -32,10 +32,117 @@ async function run() {
         // api
         app.post('/issues', async (req, res) => {
             const issue = req.body;
-            issue.createdAt = new Date();
             const result = await issuesCollection.insertOne(issue);
             res.send(result);
         });
+
+        // issue get api
+        app.get('/issues', async (req, res) => {
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 9;
+        const skip = (page - 1) * limit;
+        const search = req.query.search || '';
+        const status = req.query.status;
+        const priority = req.query.priority;
+        const category = req.query.category;
+        const query = {};
+        if (search) {
+            query.$or = [
+                { title: { $regex: search, $options: 'i' } },
+                { category: { $regex: search, $options: 'i' } },
+                { location: { $regex: search, $options: 'i' } }
+            ];
+        }
+        if (status) {
+            query.status = status;
+        }
+        if (priority) {
+            query.isBoosted = priority.toLowerCase() === 'high' ? true : false;
+        }
+        if (category) {
+            query.category = { $regex: category, $options: 'i' }; // <-- fixed
+        }
+        const total = await issuesCollection.countDocuments(query);
+        const totalPages = Math.ceil(total / limit);
+        const issues = await issuesCollection
+            .find(query)
+            .sort({ isBoosted: -1, priority: -1, upvotes: -1, createdAt: -1 })
+            .skip(skip)
+            .limit(limit)
+            .toArray();
+        res.send({ issues, totalPages });
+    });
+
+
+
+
+        // app.get('/issues', async (req, res) => {
+        //     const page = parseInt(req.query.page) || 1;
+        //     const limit = parseInt(req.query.limit) || 9;
+        //     const skip = (page - 1) * limit;
+        //     const total = await issuesCollection.countDocuments();
+        //     const totalPages = Math.ceil(total / limit);
+        //     const issues = await issuesCollection
+        //         .find({})
+        //         .sort({ isBoosted: -1, priority: -1, upvotes: -1, createdAt: -1 }) // sorting
+        //         .skip(skip)
+        //         .limit(limit)
+        //         .toArray();
+        //     res.send({ issues, totalPages });
+        // });
+
+
+
+
+
+        // Recently Resolved Api
+        app.get('/recent-resolved-issues', async (req, res) =>{
+            const cursor = issuesCollection
+                .find({ status: "resolved" })
+                .sort({ createdAt: -1 })
+                .limit(6);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // Toggle Upvote API
+        app.patch('/issues/upvote/:id', async (req, res) => {
+            const issueId = req.params.id;
+            const { userEmail } = req.body;
+            const issue = await issuesCollection.findOne({ _id: new ObjectId(issueId) });
+            if (!issue) return res.status(404).send({ message: "Issue not found" });
+            const hasLiked = issue.upvotedUsers?.includes(userEmail);
+            let updateDoc;
+            if (hasLiked) {
+                updateDoc = {
+                    $inc: { upvotes: -1 },
+                    $pull: { upvotedUsers: userEmail }
+                };
+            } else {
+                updateDoc = {
+                    $inc: { upvotes: 1 },
+                    $push: { upvotedUsers: userEmail }
+                };
+            }
+            const result = await issuesCollection.updateOne(
+                { _id: new ObjectId(issueId) },
+                updateDoc
+            );
+            res.send({ updated: true, liked: !hasLiked });
+        });
+
+        // Get single issue by ID
+        app.get('/issues/:id', async (req, res) => {
+            const id = req.params.id;
+            const issue = await issuesCollection.findOne({ _id: new ObjectId(id) });
+            res.send(issue);
+        });
+
+
+
+
+
+
 
 
 
