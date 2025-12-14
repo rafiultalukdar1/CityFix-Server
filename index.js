6e70ae398545f864cd6ff227c27c0ec098ed572e
rafi0012 <rafiultalukdar007@gmail.com>
2025-12-14 22:56:32 +0600

    Added citizen's issues patch api

--- index.js
+++ index.js
@@ -233,20 +233,6 @@ async function run() {
             res.send(result);
         });
 
-        // DELETE: Delete Issue
-        app.delete('/issues/:id', verifyFBToken, async (req, res) => {
-            const id = req.params.id;
-            const email = req.decoded_email;
-            const issue = await issuesCollection.findOne({ _id: new ObjectId(id) });
-            if (!issue) {
-                return res.status(404).send({ message: 'Issue not found' });
-            }
-            if (issue.submittedBy !== email) {
-                return res.status(403).send({ message: 'Forbidden access' });
-            }
-            const result = await issuesCollection.deleteOne({ _id: new ObjectId(id) });
-            res.send(result);
-        });
 
 
 
